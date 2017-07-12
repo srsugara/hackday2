8de9d1e56a1a32457402c38803c898436693ffa6
muhammadaa <muh.ari.anggara@gmail.com>
2017-07-12 17:01:30 +0700

    #6 change color style

--- app/views/Story/index.js
+++ app/views/Story/index.js
@@ -12,12 +12,13 @@ export default class storyBook extends Component {
     return (
       <View style={styles.container}>
         <View style={styles.body}>
-          <Text style={styles.titleStory}>Tittle Story</Text>
+          <Text style={styles.title}>Story Draft</Text>
+          <Text style={styles.titleStory}>Your Tittle :</Text>
           <TextInput
             style={styles.writeTitleStory}
             placeholder="write your title story..."
           />
-          <Text style={[styles.titleStory, styles.padding]}>Your Story</Text>
+          <Text style={styles.titleStory}>Your Story :</Text>
           <TextInput
             style={styles.writeStory}
             placeholder="write your story...."
@@ -28,7 +29,7 @@ export default class storyBook extends Component {
           <Button
             title="Share Story"
             color="#12005e"
-            onPress={} //this for trigger save story
+            // onPress={} //this for trigger save story
           />
         </View>
       </View>
@@ -46,17 +47,23 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#7c43bd',
   },
+  title: {
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
+    padding: 24,
+    textAlign: 'center',
+    backgroundColor: '#12005e',
+  },
   titleStory: {
     fontSize: 24,
     color: 'white',
     fontWeight: 'bold',
+    padding: 8,
   },
   writeTitleStory: {
     backgroundColor: 'white',
     fontSize: 16,
-  },
-  padding: {
-    paddingTop: 24,
   },
   writeStory: {
     flex: 1,
